f64d3ce9fc72afd0cef726885228246bd0ecfd67
Nanako7-ix <nanako7.ix@gmail.com>
2025-05-29 13:39:59 +0800

    AhoCorasick

--- String/AhoCorasick.cpp
+++ String/AhoCorasick.cpp
@@ -4,8 +4,9 @@ struct AhoCorasick {
     static constexpr int SIGMA = 26;
     struct Node {
         int len, fail;
+        std::vector<int> adj;
         std::array<int, SIGMA> next;
-        Node() : len(0), fail(0), next{} {}
+        Node() : len(0), fail(0), adj{}, next{} {}
     };
 
     std::vector<Node> t;
@@ -16,6 +17,7 @@ struct AhoCorasick {
         t.assign(2, Node());
         t[0].next.fill(1);
         t[0].len = -1;
+        t[0].adj.push_back(1);
     }
     
     int newNode() {
@@ -41,30 +43,35 @@ struct AhoCorasick {
         q.push(1);
         
         while (!q.empty()) {
-            int x = q.front();
+            int u = q.front();
             q.pop();
             
             for (int i = 0; i < SIGMA; i++) {
-                if (t[x].next[i] == 0) {
-                    t[x].next[i] = t[t[x].fail].next[i];
+                if (t[u].next[i] == 0) {
+                    t[u].next[i] = t[t[u].fail].next[i];
                 } else {
-                    t[t[x].next[i]].fail = t[t[x].fail].next[i];
-                    q.push(t[x].next[i]);
+                    t[t[u].next[i]].fail = t[t[u].fail].next[i];
+                    t[t[t[u].fail].next[i]].adj.push_back(t[u].next[i]);
+                    q.push(t[u].next[i]);
                 }
             }
         }
     }
     
-    int next(int p, int x) {
-        return t[p].next[x];
+    int len(int p) {
+        return t[p].len;
     }
     
     int fail(int p) {
         return t[p].fail;
     }
     
-    int len(int p) {
-        return t[p].len;
+    const std::vector<int>& adj(int p) {
+        return t[p].adj;
+    }
+    
+    int next(int p, int x) {
+        return t[p].next[x];
     }
     
     int size() {
