70b464216f5706b2369a1a97fd15424008bcdde3
Nanako7-ix <nanako7.ix@gmail.com>
2025-06-01 17:34:56 +0800

    KMP

--- String/kmpAlgorithm.cpp
+++ String/kmpAlgorithm.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 
-struct KMP : std::string {
+struct kmpAlgorithm : std::string {
     int n;
     std::vector<int> fail;
-    KMP (std::string s) : std::string(s), n(s.size()), fail(n) {
+    kmpAlgorithm (std::string s) : std::string(s), n(s.size()), fail(n) {
         for (int i = 1, j = 0; i < n; ++i) {
             while (j && s[i] != s[j]) j = fail[j - 1];
             fail[i] = j += (s[i] == s[j]);
@@ -11,7 +11,7 @@ struct KMP : std::string {
     }
 };
 
-auto match(std::string s, KMP& p) {
+auto match(std::string s, kmpAlgorithm& p) {
     int n = s.size();
     std::vector<std::array<int, 2>> ans;
     for (int i = 0, j = 0; i < n; ++i) {
