d6d418367d1971dc6c90952f0b2cec02a85f92ea
Nanako7-ix <nanako7.ix@gmail.com>
2025-06-03 22:15:09 +0800

    gdcpc

--- TEST/Manacher/gdcpc.cpp
+++ TEST/Manacher/gdcpc.cpp
@@ -45,6 +45,7 @@ auto Manacher(const std::string& t, char wildcard = '*') {
     for (auto c : t) {
         s += c, s += '#';
     }
+
     auto check = [&](char x, char y) {
         return x == y || x == wildcard || y == wildcard;
     };
