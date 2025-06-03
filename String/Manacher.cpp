2f349cb26e104f96476be97c90e2b22ceb0e8579
Nanako7-ix <nanako7.ix@gmail.com>
2025-06-03 18:40:54 +0800

    Manacher Update

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -1,15 +1,19 @@
 #include <bits/stdc++.h>
 
-auto Manacher(const std::string& t) {
+auto Manacher(const std::string& t, const char wildcard = '*') {
     std::string s = "#";
     for (auto c : t) {
         s += c, s += '#';
     }
+    auto check = [&](char x, char y) {
+        return x == y || x == wildcard || y == wildcard;
+    };
+
     int n = s.size();
     std::vector<int> f(n);
     for (int i = 0, j = 0; i < n; ++i) {
         f[i] = i < j + f[j] ? std::min(f[2 * j - i], j + f[j] - i) : 1;
-        while (i + f[i] < n && i - f[i] >= 0 && s[i + f[i]] == s[i - f[i]]) ++f[i];
+        while (i + f[i] < n && i - f[i] >= 0 && check(s[i + f[i]], s[i - f[i]])) ++f[i];
         if (i + f[i] > j + f[j]) {
             for (int k = std::max(i, j + f[j]); k < i + f[i]; ++k) {
                 if (s[k] != '#') continue;
@@ -18,5 +22,6 @@ auto Manacher(const std::string& t) {
             j = i;
         }
         auto [l, r] = std::array { (i - f[i] + 1) / 2, (i + f[i]) / 2 - 1 };
+        if (l > r) continue;
     }
 }
