b441f3982cc607cdd345663188431da1b67148e7
Nanako7-ix <nanako7.ix@gmail.com>
2025-06-05 00:23:39 +0800

    "Manacher"

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 
-auto Manacher(const std::string& t, const char wildcard = '*') {
+template<typename Func = std::equal_to<char>>
+auto Manacher(const std::string& t, Func check) {
     std::string s = "#";
     for (auto c : t) {
         s += c, s += '#';
     }
-    
-    auto check = [&](char x, char y) {
-        return x == y || x == wildcard || y == wildcard;
-    };
 
     int n = s.size();
     std::vector<int> f(n);
@@ -24,5 +21,22 @@ auto Manacher(const std::string& t, const char wildcard = '*') {
         }
         auto [l, r] = std::array { (i - f[i] + 1) / 2, (i + f[i]) / 2 - 1 };
         if (l > r) continue;
+    }
+}
+
+template<typename Func = std::equal_to<char>>
+auto Manacher(const std::string& s, Func&& check = Func {}) {
+    int n = s.size();
+    std::vector<int> f(n);
+    for (int i = 0, j = 0; i < n; ++i) {
+        f[i] = i < j + f[j] ? std::min(f[2 * j - i], j + f[j] - i) : 1;
+        while (i + f[i] < n && i - f[i] >= 0 && check(s[i + f[i]], s[i - f[i]])) ++f[i];
+        if (i + f[i] > j + f[j]) {
+            for (int k = std::max(i, j + f[j]); k < i + f[i]; ++k) {
+                auto [l, r] = std::array { 2 * i - k , k };
+            }
+            j = i;
+        }
+        auto [l, r] = std::array { i - f[i] + 1, i + f[i] - 1 };
     }
 }
