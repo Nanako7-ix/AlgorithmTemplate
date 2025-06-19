ce1a9b8757003592c759aebafef2a3b1e1889100
Nanako7-ix <nanako7.ix@gmail.com>
2025-06-19 18:33:57 +0800

    114514

---
+++ DataStructure/SparseTable.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+template<typename T, typename Func>
+struct SparseTable {
+    int n;
+    Func op;
+    std::vector<std::vector<T>> st;
+
+    SparseTable () = default;
+
+    template<typename Iterator, typename = std::_RequireInputIter<Iterator>>
+    SparseTable (const Iterator& l, const Iterator& r, Func&& f) : op(f) {
+        init(l, r);
+    }
+
+    template<typename Iterator, typename = std::_RequireInputIter<Iterator>>
+    void init (const Iterator& l, const Iterator& r) {
+        n = r - l;
+        st.assign(std::__lg(n) + 1, std::vector<T> (n + 1));
+        for (int i = 1; i <= n; ++i) {
+            st[0][i] = *(l + i - 1);
+        }
+        for (int i = 1; i <= std::__lg(n); ++i) {
+            for (int j = 1; j + (1 << i) - 1 <= n; ++j) {
+                st[i][j] = op(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+            }
+        }
+    }
+
+    T operator () (int l, int r) {
+        assert(l <= r);
+        int k = std::__lg(r - l + 1);
+        return op(st[k][l], st[k][r - (1 << k) + 1]);
+    }
+
+    T query (int l, int r) {
+        T res {};  
+        int len = r - l + 1;
+        for (int i = 0, k = l; i <= std::__lg(len); ++i) {
+            if(len >> i & 1) {
+                res = k == l ? st[i][l] : op(res, st[i][k]);
+                k += 1 << i;
+            }
+        }
+        return res;
+    }
+};
