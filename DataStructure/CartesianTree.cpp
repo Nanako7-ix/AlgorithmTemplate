76cf109045082a884ebc5d8d9fe34eb66533140c
Nanako7-ix <nanako7.ix@gmail.com>
2025-08-12 21:23:35 +0800

    笛卡尔树

---
+++ DataStructure/CartesianTree.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+// f(a[fa], a[son]) = true
+template<typename Iterator, typename Func, typename = std::_RequireInputIter<Iterator>>
+auto getCartesianTree(const Iterator& l, const Iterator& r, Func&& f) {
+    int n = r - l;
+    std::vector<int> ls(n + 1), rs(n + 1), fa(n + 1);
+    std::vector<int> stk;
+
+    auto a = [&](int p) {
+        return *(l + p - 1);
+    };
+
+    for(int i = 1; i <= n; ++i) {
+        while(!stk.empty() && f(a(i), a(stk.back()))) {
+            ls[i] = stk.back();
+            stk.pop_back();
+        }
+        fa[ls[i]] = i;
+        if(!stk.empty()) {
+            rs[stk.back()] = i;
+            fa[i] = stk.back();
+        }
+        stk.push_back(i);
+    }
+
+    return std::tuple {
+        stk[0],
+        std::move(ls),
+        std::move(rs),
+        std::move(fa)
+    };
+}
